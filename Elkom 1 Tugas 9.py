25b78ad7984cc694af28c3c68d80535ba219512d
Quina12 <91168308+Quina12@users.noreply.github.com>
2021-11-25 00:25:31 +0700

    Add files via upload

---
+++ Elkom 1 Tugas 9.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 20:29:31 2021
+
+@author: Quina
+"""
+
+print(" @@@    @   @  @@@  @    @      @@")
+print("@   @   @   @   @   @@   @     @  @")
+print("@ @ @   @   @   @   @ @  @    @@@@@@")
+print("@   @   @   @   @   @  @ @   @      @")
+print(" @@@ @  @ @ @  @@@  @    @  @        @")
+
+Angka = (11, 13, 17, 21, 27, 30)
+List = list(Angka)
+print("LIST:", List)
+print("HASIL REVERSE LIST MENJADI TUPLE: ",Angka)
